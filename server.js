0a826032b02a9375e797a96d283a79a9263bc981
makelabsdinesh <makelabsdinesh@gmail.com>
2025-07-14 13:31:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Share this link with friends to join meetings!`);
